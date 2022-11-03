871a03f2fb441195006fb55a0844f2ffee2b7268
ukrocks007 <ukrocks.mehta@gmail.com>
2022-11-03 08:04:39 +0530

    minor fix

--- routes/user.js
+++ routes/user.js
@@ -20,14 +20,21 @@ router.post('/user/login', async (req, res) => {
                 ]
             }
         });
+        if(!user) {
+            res.status(400).json({
+                success: false,
+                message: 'Invalid email or password'
+            });
+        } else {
         user.password = undefined;
         const token = jwt.sign(JSON.parse(JSON.stringify(user)), config.jwtSecret, { expiresIn: `${24 * 60 * 60} s` });
         res.status(user ? 200 : 404).json({
             success: !!user,
             user,
             token,
-            message: user ? 'LoggedIn' : 'Invalid email or password'
+            message: 'LoggedIn'
         });
+    }
     } catch (ex) {
         console.log(ex);
         res.status(400).json(ex);
